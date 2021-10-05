648daf0623ddd8c368f6ccd2537436da947b3d23
Samuele <61940773+samudg7@users.noreply.github.com>
2021-10-06 00:06:08 +0200

    token

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:samudg7/test-deploy1.git master:gh-pages
+git push -f git@github.com:samudg7/test-deploy1.git develop:gh-pages
 
 cd -
