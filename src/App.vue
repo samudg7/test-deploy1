2b2743c33a166239847964c20f8ba894d8ad6eb7
Samuele <61940773+samudg7@users.noreply.github.com>
2021-10-06 12:19:45 +0200

    modifiche

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your test deploy 1.3 Vue.js App"/>
+    <HelloWorld msg="Welcome to Your test deploy 1.5 Vue.js App"/>
   </div>
 </template>
 
